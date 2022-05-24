b3dcfdbc58f2a142c6c7f220cb715f35a3842a99
Cooper Weiser <cooper@datacrunchcorp.com>
2022-05-23 20:13:10 -0700

    Add retrieve endpoint

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ app.get('/todos', (req, res) => {
     res.send(todos);
 })
 
+app.get('/todos/:id', (req, res) => {
+    if (req.params.id > 3 || req.params.id < 1){
+        res.status(404);
+        res.send("Todo with id " + req.params.id + " does not exist.");
+    } else {
+        res.send(todos[req.params.id - 1]);
+    }
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
