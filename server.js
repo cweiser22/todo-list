1a96faf67d78a657d1418006cf81e1fdfce8a32a
Cooper Weiser <cooper@datacrunchcorp.com>
2022-05-23 19:59:41 -0700

    Add npm start

--- server.js
+++ server.js
@@ -2,9 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const todos = [
+    {
+        id: 1,
+        task: 'Get groceries',
+    },
+    {
+        id: 2,
+        task: 'Order Chinese food',
+    },
+    {
+        id: 3,
+        task: 'Read Tolkien',
+    },
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/todos', (req, res) => {
+    res.send(todos);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
